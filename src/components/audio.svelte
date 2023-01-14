9230fa9990e62249027b96d9cdbe49e4a1784f21
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2023-01-14 14:17:05 -0500

    audio tag stuff

---
+++ src/components/audio.svelte
@@ -0,0 +1,16 @@
+<script>
+
+    export let musicSource = String;
+
+    function Slider() {
+
+    }
+
+</script>
+
+<main>
+    <audio src={musicSource}></audio>
+    <div>
+        <input type="range" step="1" class="w-96" on:drag={Slider}>
+    </div>
+</main>
